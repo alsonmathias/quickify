612ea66de0573b6f5619b129c6ba507debdfd332
alsonmathias <alsonmathias1209@gmail.com>
2025-08-10 00:20:54 +0530

    Completed Feed frontend

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { dummyPostsData } from "../assets/assets";
+import { assets, dummyPostsData } from "../assets/assets";
 import Loading from "../components/Loading";
 import StoriesBar from "../components/StoriesBar";
+import PostCard from "../components/PostCard";
+import RecentMessages from "../components/RecentMessages";
 
 const Feed = () => {
   const [feeds, setFeeds] = useState([]);
@@ -17,15 +19,29 @@ const Feed = () => {
     <div className="h-full overflow-y-scroll py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
       {/* Stories and post list */}
       <div className="">
-        <StoriesBar/>
-        <div className="p-4 space-y-6">List of post</div>
+        <StoriesBar />
+        <div className="p-4 space-y-6">
+          {feeds.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
+        </div>
       </div>
       {/* Right Sidebar */}
-      <div className="">
-        <div>
-          <h1>Sponsored</h1>
+      <div className="max-xl:hidden sticky top-0">
+        <div className="max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow">
+          <h3 className="text-slate-800 font-semibold">Sponsored</h3>
+          <img
+            src={assets.sponsored_img}
+            alt=""
+            className="w-75 h-50 rounded-md"
+          />
+          <p className="text-slate-600">Email Marketing</p>
+          <p className="text-slate-400">
+            Supercharge your marketing with a powerful, easy-to-use platform
+            built for results.
+          </p>
         </div>
-        <h1>Recent Messages</h1>
+        <RecentMessages/>
       </div>
     </div>
   ) : (
